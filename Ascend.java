853b3c095e8cfcde0d997becb42e165d1a7fce8d
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-11-06 17:33:53 +0200

    assigment2

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,24 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		
+		int max = Integer.parseInt(args[0]);
+		double a =(Math.random() * max);
+		double b =(Math.random() * max);
+		double c =(Math.random() * max);
+
+		System.out.println(" " + (int)(a)+ " " + (int)(b) + " " + (int)(c));
+
+		double min1 = Math.min(a,b);
+		double min2 = Math.min(min1,c);
+		double max1 = Math.max(a, b);
+		double max2 = Math.max(max1, c);
+		double mid = ((a+b+c)- (min2+max2));
+		System.out.println(" " + (int)(min2)+ " " + (int)(mid) + " " + (int)(max2));
+
+
+
+
 	}
 }
+
